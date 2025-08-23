dbb504472da917af883f2e494a7a94b9424530fb
xuxueli <931591021@qq.com>
2025-08-24 00:57:16 +0800

    update doc

--- xxl-boot-admin/src/main/java/com/xxl/boot/admin/web/error/WebHandlerExceptionResolver.java
+++ xxl-boot-admin/src/main/java/com/xxl/boot/admin/web/error/WebHandlerExceptionResolver.java
@@ -3,6 +3,8 @@ package com.xxl.boot.admin.web.error;
 import com.xxl.tool.exception.BizException;
 import com.xxl.tool.gson.GsonTool;
 import com.xxl.tool.response.Response;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -11,8 +13,6 @@ import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.ModelAndView;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
