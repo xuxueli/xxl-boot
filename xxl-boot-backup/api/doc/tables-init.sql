b104040a171c2d169f7f99f29f71aeb9270e2072
xuxueli <931591021@qq.com>
2026-07-12 05:31:25 +0800

    update

--- xxl-boot-backup/api/doc/tables-init.sql
+++ xxl-boot-backup/api/doc/tables-init.sql
@@ -165,7 +165,7 @@ create table sys_menu (
 -- 初始化-菜单信息表数据
 -- ----------------------------
 -- 一级菜单
-insert into sys_menu values('5', '首页', '0', '0', 'index',           'index', '', '', 1, 0, 'M', '0', '0', '', 'dashboard',   'admin', sysdate(), '', null, '首页');
+insert into sys_menu values('5', '首页', '0', '0', 'index',      'index', '', '', 1, 0, 'C', '0', '0', '', 'dashboard',   'admin', sysdate(), '', null, '首页');
 insert into sys_menu values('1', '组织管理', '0', '1', 'system',           null, '', '', 1, 0, 'M', '0', '0', '', 'system',   'admin', sysdate(), '', null, '系统管理目录');
 insert into sys_menu values('2', '系统管理', '0', '2', 'monitor',          null, '', '', 1, 0, 'M', '0', '0', '', 'monitor',  'admin', sysdate(), '', null, '系统监控目录');
 insert into sys_menu values('3', '系统工具', '0', '3', 'tool',             null, '', '', 1, 0, 'M', '0', '0', '', 'tool',     'admin', sysdate(), '', null, '系统工具目录');
