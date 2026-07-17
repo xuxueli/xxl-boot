d7288cff4f9cca8b25545f758919ae55bf18cfb8
xuxueli <931591021@qq.com>
2026-07-18 00:13:32 +0800

    update

--- xxl-boot-backup/ui/src/components/TreePanel/index.vue
+++ xxl-boot-backup/ui/src/components/TreePanel/index.vue
@@ -72,7 +72,7 @@
 </template>
 
 <script setup>
-import { ArrowDown, ArrowUp, DArrowLeft, DArrowRight, Document, Folder, OfficeBuilding, Refresh } from '@element-plus/icons-vue'
+import { ArrowDown, ArrowUp, DArrowLeft, DArrowRight, Document, Folder, OfficeBuilding, Refresh, Search } from '@element-plus/icons-vue'
 
 const props = defineProps({
   // 树形数据
