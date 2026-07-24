effea93281948e4390f748f97267f5f9221a944a
xuxueli <931591021@qq.com>
2026-07-24 21:23:42 +0800

    update

--- xxl-boot-ui/src/views/login.vue
+++ xxl-boot-ui/src/views/login.vue
@@ -1,7 +1,12 @@
+<!--
+  页面：Login（登录页面）
+  功能：账号密码登录、验证码获取与展示、记住密码（Cookie 回填）
+-->
 <template>
   <div class="login">
     <el-form ref="loginRef" :model="loginForm" :rules="loginRules" class="login-form">
       <h3 class="title">{{ title }}</h3>
+      <!-- 用户名 -->
       <el-form-item prop="username">
         <el-input
           v-model="loginForm.username"
@@ -13,6 +18,7 @@
           <template #prefix><SvgIcon icon-class="user" class="el-input__icon input-icon" /></template>
         </el-input>
       </el-form-item>
+      <!-- 密码 -->
       <el-form-item prop="password">
         <el-input
           v-model="loginForm.password"
@@ -25,6 +31,7 @@
           <template #prefix><SvgIcon icon-class="password" class="el-input__icon input-icon" /></template>
         </el-input>
       </el-form-item>
+      <!-- 验证码 -->
       <el-form-item prop="code" v-if="captchaEnabled">
         <el-input
           v-model="loginForm.code"
@@ -40,6 +47,7 @@
           <img :src="codeUrl" @click="getCode" class="login-code-img"/>
         </div>
       </el-form-item>
+      <!-- 记住密码 -->
       <el-checkbox v-model="loginForm.rememberMe" style="margin:0px 0px 25px 0px;">记住密码</el-checkbox>
       <el-form-item style="width:100%;">
         <el-button
@@ -65,19 +73,21 @@
 </template>
 
 <script setup>
+// 引入
 import { getCodeImg } from "@/api/login"
 import Cookies from "js-cookie"
 import { encrypt, decrypt } from "@/utils/jsencrypt"
 import { useUserStore } from '@/store'
 import defaultSettings from '@/settings'
 
-const title = defaultSettings.title
-const footerContent = defaultSettings.footerContent
+const title = defaultSettings.title                   // 系统标题
+const footerContent = defaultSettings.footerContent   // 页脚版权信息
 const userStore = useUserStore()
-const route = useRoute()
-const router = useRouter()
-const loginRef = ref(null)
+const route = useRoute()                              // 路由
+const router = useRouter()                            // 路由
+const loginRef = ref(null)                            // 登录表单 ref
 
+// 登录表单数据
 const loginForm = ref({
   username: "admin",
   password: "admin123",
@@ -86,40 +96,49 @@ const loginForm = ref({
   uuid: ""
 })
 
+// 表单校验规则
 const loginRules = {
   username: [{ required: true, trigger: "blur", message: "请输入您的账号" }],
   password: [{ required: true, trigger: "blur", message: "请输入您的密码" }],
   code: [{ required: true, trigger: "change", message: "请输入验证码" }]
 }
 
-const codeUrl = ref("")
-const loading = ref(false)
-// 验证码开关
-const captchaEnabled = ref(true)
-// 注册开关
-const register = ref(false)
-const redirect = ref(undefined)
+const codeUrl = ref("")                   // 验证码图片 base64
+const loading = ref(false)                // 登录按钮 loading
+const captchaEnabled = ref(true)          // 验证码开关
+const register = ref(false)               // 注册开关
+const redirect = ref(undefined)           // 登录后重定向地址
 
+
+// 监听路由参数，获取重定向地址
 watch(route, (newRoute) => {
     redirect.value = newRoute.query && newRoute.query.redirect
 }, { immediate: true })
 
+
+/**
+ * 处理登录：
+ *    - 表单校验
+ *    - →记住密码（写入 Cookie）
+ *    - →调用登录接口
+ *    - →路由跳转
+ */
 function handleLogin() {
   loginRef.value.validate(valid => {
     if (valid) {
       loading.value = true
-      // 勾选了需要记住密码设置在 cookie 中设置记住用户名和密码
+      // 勾选记住密码→写入 Cookie
       if (loginForm.value.rememberMe) {
         Cookies.set("username", loginForm.value.username, { expires: 30 })
         Cookies.set("password", encrypt(loginForm.value.password), { expires: 30 })
         Cookies.set("rememberMe", loginForm.value.rememberMe, { expires: 30 })
       } else {
-        // 否则移除
+        // 未勾选→移除 Cookie
         Cookies.remove("username")
         Cookies.remove("password")
         Cookies.remove("rememberMe")
       }
-      // 调用action的登录方法
+      // 执行登录
       userStore.login(loginForm.value).then(() => {
         const query = route.query
         const otherQueryParams = Object.keys(query).reduce((acc, cur) => {
@@ -140,6 +159,7 @@ function handleLogin() {
   })
 }
 
+/** 获取验证码图片，根据开关控制显示 */
 function getCode() {
   getCodeImg().then(res => {
     captchaEnabled.value = res.captchaEnabled === undefined ? true : res.captchaEnabled
@@ -150,6 +170,7 @@ function getCode() {
   })
 }
 
+/** 从 Cookie 中读取已记住的用户名、密码，回填表单 */
 function getCookie() {
   const username = Cookies.get("username")
   const password = Cookies.get("password")
@@ -161,9 +182,13 @@ function getCookie() {
   }
 }
 
+// 获取验证码
 getCode()
+
+// 回填已记住的凭据
 getCookie()
 </script>
+
 
 <style lang='scss' scoped>
 .login {
