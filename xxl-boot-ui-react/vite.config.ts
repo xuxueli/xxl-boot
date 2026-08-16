63ba749a91594d96877e8cbbce166c9205a4bc69
xuxueli <931591021@qq.com>
2026-08-16 14:52:49 +0800

    refactor: improve code formatting and consistency across multiple files

--- xxl-boot-ui-react/vite.config.ts
+++ xxl-boot-ui-react/vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig, loadEnv } from 'vite';
  * Vite 构建配置
  * 环境变量：.rawEnv.development / .rawEnv.production / .rawEnv.staging
  */
-export default defineConfig(({ mode }) => {
+export default defineConfig(({ mode, command }) => {
   /**
    * 获取环境变量：
    */
@@ -14,6 +14,9 @@ export default defineConfig(({ mode }) => {
   const API_URL = rawEnv.VITE_API_URL || 'http://localhost:8081';
   const APP_BASE_API = rawEnv.VITE_APP_BASE_API || '/api';
   const APP_PORT = Number(rawEnv.VITE_APP_PORT) || 4000;
+
+  // 是否为生产环境：
+  const isBuild = command === 'build';
 
   /**
    * Vite 配置项：
@@ -36,26 +39,39 @@ export default defineConfig(({ mode }) => {
       extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json'], // 配置文件扩展名，允许在导入时省略扩展名
     },
     /**
-     * 构建配置 (Build Options)
-     *  - outDir: 输出目录，默认 dist
-     *  - chunkSizeWarningLimit: 分块大小警告限制，单位 KB，默认 500
+     * 构建配置：
+     *    作用：构建优化，如：代码分割、静态资源处理、压缩、优化第三方依赖项
+     *    文档：https://cn.vitejs.dev/config/#build-options
      */
     build: {
-      outDir: 'dist',
-      chunkSizeWarningLimit: 2048,
+      sourcemap: isBuild ? false : 'inline',                   // 生产环境打包时不生成Source Map；减小打包体积，提高加载速度；防止源代码泄露；
+      outDir: 'dist',                                          // 构建输出目录
+      assetsDir: 'assets',                                     // 静态资源目录
+      chunkSizeWarningLimit: 2000,                             // 构建时超过指定大小会警告
+      rollupOptions: {                                         // rollup 配置
+        output: {
+          chunkFileNames: 'static/js/[name]-[hash].js',
+          entryFileNames: 'static/js/[name]-[hash].js',
+          assetFileNames: 'static/[ext]/[name]-[hash].[ext]'
+        }
+      }
     },
     /**
      * Vite 开发服务器（Dev Server）的代理配置‌：
      */
     server: {
       port: APP_PORT,
-      host: true,
-      open: true,
+      strictPort: true,     // 端口被占用时直接退出
+      host: true,           // 默认是localhost
+      open: true,           // 运行自动打开浏览器
       proxy: {
-        // 开发代理：/api -> 后端地址，后端无 /api 前缀需剥离
+        // 请求前缀匹配
         [APP_BASE_API]: {
+          // 后端API地址
           target: API_URL,
+          // 请求头 Origin/Host 改目标地址域名
           changeOrigin: true,
+          // 请求前缀移除
           rewrite: (path) => path.replace(new RegExp(`^${APP_BASE_API}`), ''),
         },
       },
